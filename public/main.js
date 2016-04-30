2e9bcabe7632b236ea7e5c8176067de7b531d8e3
Aubrey Ford <musicaubrey@gmail.com>
2016-04-29 20:21:41 -0600

    whatever/

--- public/main.js
+++ public/main.js
@@ -9,4 +9,27 @@ angular.module("main")
 			// console.log(returnData);
 		})
 	}
+
+	$scope.shakestatus = "stay"
+
+	$scope.shake = function(){
+
+		$scope.shakestatus = "shake"
+
+		setTimeout(function(){
+
+			console.log("asdfgasdf")
+			if($scope.shakestatus === "shake"){
+
+				$scope.shakestatus = "stay"
+
+			}else{
+
+				$scope.shakestatus = "shake"
+
+			}
+		}, 1000)
+
+	}
+
 }])
